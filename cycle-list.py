47d52e994bf9065e6f75460cbe122ee903789824
Luke Holman <holman57@gmail.com>
2024-05-10 02:15:43 -0500

    Update cycle-list.py

--- cycle-list.py
+++ cycle-list.py
@@ -35,8 +35,16 @@ def wait_key():
     return result
 
 
-def cycle(url):
-    print("\n\n\t", url, "\n\n\n\t\tPress Any Key...")
+def cycle(url, cycle_iteration, total_iterations):
+    print(
+        "\n\n\tcycle:",
+        cycle_iteration,
+        "\t\ttotal iterations:",
+        total_iterations,
+        "\n\n\t",
+        url,
+        "\n\n\n\t\tPress Any Key...",
+    )
     a = wait_key()
     if a == "\x1b" or a == "q":
         exit(0)
@@ -44,7 +52,9 @@ def cycle(url):
     cls()
 
 
+c = 0
 while True:
+    c += 1
     cls()
     parser = argparse.ArgumentParser(prog="Cycle List", description="!", epilog="_")
     parser.add_argument("filename")
@@ -60,12 +70,12 @@ while True:
     random.shuffle(priority3)
     loop_list = priority2 + priority3
     i = 0
-    for page in tqdm(priority2):
+    for page in tqdm(loop_list):
         if i % 7 == 0:
             random_priority1 = random.choice(priority1)
-            cycle(random_priority1)
+            cycle(random_priority1, c, i)
         i += 1
-        cycle(page)
+        cycle(page, c, i)
         continue
     cls()
     print()
